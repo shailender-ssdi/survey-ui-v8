522f2ba50221993fb8e4c68eacb17ce9cd3e65ab
Valupadasu <Vinay.Valupadasu@hhs.texas.gov>
2023-05-18 09:27:34 -0400

    update config to point to dev fhir server

--- source/js/config.js
+++ source/js/config.js
@@ -7,7 +7,8 @@ export const config = {
   formContainer: 'formContainer',
   //fhirServerUrl: 'http://ec2-3-136-234-37.us-east-2.compute.amazonaws.com:8080/fhir'
   //fhirServerUrl: 'https://lforms-fhir.nlm.nih.gov/baseR4/'
-  fhirServerUrl: 'http://100.92.2.230:8090/fhir',
+  //fhirServerUrl: 'http://100.92.2.230:8090/fhir',
+  fhirServerUrl: 'http://100.92.3.16:8090/fhir',
     /**
    * Whether to try to resend a request without Cache-Control if a request with
    * Cache-Control returns an error.
